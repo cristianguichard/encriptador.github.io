47f48176f465c16d51879b7f0c574f707fdbae2d
estudio315 <suscripciones@315.cl>
2022-08-15 10:17:57 -0400

    se ha creado la funcion copyToClickBoard

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -77,44 +77,15 @@ function encriptar() {
 function desencriptar() {
     var input = document.querySelector("input");
     var texto = input.value;
-    var largoTexto = texto.length;
-    var textoEncriptado = "";
-   
-     for (var posicion = 0; posicion < largoTexto; posicion++){
-      
-        if (recorrerVocales(texto[posicion])){
- 
-         var vocal = recorrerVocales(texto[posicion]);
- 
-             if (vocal == "a"){
-                 var a = "ai";
-                 var textoEncriptado = textoEncriptado + a;
-             } else if (vocal == "e"){
-                 var e = "enter";
-                 var textoEncriptado = textoEncriptado + e;
-             } else if (vocal == "i"){
-                 var i = "imes";
-                 var textoEncriptado = textoEncriptado + i;
-             } else if (vocal == "o"){
-                 var o = "ober";
-                 var textoEncriptado = textoEncriptado + o;
-             } else if (vocal == "u"){
-                 var u = "ufat";
-                 var textoEncriptado = textoEncriptado + u;
-             }
- 
-        } else {
-          var textoEncriptado = textoEncriptado + texto[posicion];
-         }
- 
-    }  
-    document.getElementById("mensaje-encriptado").innerHTML = textoEncriptado;
-       mostrar();
-       ocultar();
-      
-     
-       
- }
+    
+    var textoCifrado = texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+   var textoDesencriptado = textoCifrado;
+  
+   document.getElementById("mensaje-encriptado").innerHTML = textoDesencriptado;
+      mostrar();
+      ocultar();
+}
 function mostrar(){
     document.getElementById("caja-resultado").style.display ="flex";
 
@@ -125,12 +96,17 @@ function ocultar(){
     document.getElementById("texto-inicial").style.display = "none";
 }
 
-function copy() {
-    let copyText = document.getElementById("mensaje-encriptado");
-    copyText.select();
-    document.execCommand("copy");
-    alert("Copiado");
-  }
-//var button = document.querySelector("button");
+function copyToClickBoard(){
+    var content = document.getElementById('mensaje-encriptado').innerHTML;
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        alert("El texto" + " " + content + " " + "ha sido copiado...");
+    })
+        .catch(err => {
+        alert('algo ha pasado', err);
+    })
+ 
+}//var button = document.querySelector("button");
 
 //button.onclick = encriptar;
