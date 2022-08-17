b3e84b1cf3aa1c5beacc21704c8f89194abb840f
estudio315 <suscripciones@315.cl>
2022-08-17 13:37:05 -0400

    version grid

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -28,55 +28,79 @@ function recorrerVocales(vocal){
     }
 
 }
-
+function validar(texto) {
+ 
+        var texto = texto;
+        let letrasValidas = "abcdefjhijklmnñopqrstuvwxyz¿?¡! ";
+    
+        for (let letra of texto) {
+            if (!letrasValidas.includes(letra)) {
+               
+                validacion = 1;
+                break;
+            } else {
+                validacion = 0;
+            }
+        }
+        if (validacion == 1) {
+            return true;
+        } else {
+            return false
+        }
+    }
 
 function encriptar() {
+    var input = document.querySelector("input");
+    var texto = input.value;
+    if(validar(texto)){
+        alert("No ingreses Mayusculas ni acentos");
+   } else {
+           
+    var largoTexto = texto.length;
+    var textoEncriptado = "";
+    
+        for (var posicion = 0; posicion < largoTexto; posicion++){
+        
+        if (recorrerVocales(texto[posicion])){
 
+            var vocal = recorrerVocales(texto[posicion]);
 
-   var input = document.querySelector("input");
-   var texto = input.value;
-   var largoTexto = texto.length;
-   var textoEncriptado = "";
-  
-    for (var posicion = 0; posicion < largoTexto; posicion++){
-     
-       if (recorrerVocales(texto[posicion])){
+                if (vocal == "a"){
+                    var a = "ai";
+                    var textoEncriptado = textoEncriptado + a;
+                } else if (vocal == "e"){
+                    var e = "enter";
+                    var textoEncriptado = textoEncriptado + e;
+                } else if (vocal == "i"){
+                    var i = "imes";
+                    var textoEncriptado = textoEncriptado + i;
+                } else if (vocal == "o"){
+                    var o = "ober";
+                    var textoEncriptado = textoEncriptado + o;
+                } else if (vocal == "u"){
+                    var u = "ufat";
+                    var textoEncriptado = textoEncriptado + u;
+                }
 
-        var vocal = recorrerVocales(texto[posicion]);
-
-            if (vocal == "a"){
-                var a = "ai";
-                var textoEncriptado = textoEncriptado + a;
-            } else if (vocal == "e"){
-                var e = "enter";
-                var textoEncriptado = textoEncriptado + e;
-            } else if (vocal == "i"){
-                var i = "imes";
-                var textoEncriptado = textoEncriptado + i;
-            } else if (vocal == "o"){
-                var o = "ober";
-                var textoEncriptado = textoEncriptado + o;
-            } else if (vocal == "u"){
-                var u = "ufat";
-                var textoEncriptado = textoEncriptado + u;
-            }
-
-       } else {
-         var textoEncriptado = textoEncriptado + texto[posicion];
+        } else {
+            var textoEncriptado = textoEncriptado + texto[posicion];
         }
 
-   }  
-   document.getElementById("mensaje-encriptado").innerHTML = textoEncriptado;
-      mostrar();
-      ocultar();
+    }  
+    document.getElementById("mensaje-encriptado").innerHTML = textoEncriptado;
+        mostrar();
+        ocultar();
      
-    
-      
+    }     
 }
-
 function desencriptar() {
     var input = document.querySelector("input");
     var texto = input.value;
+
+    if(validar(texto)){
+        alert("No ingreses Mayusculas ni acentos");
+    
+       } else {
     
     var textoCifrado = texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
 
@@ -85,17 +109,14 @@ function desencriptar() {
    document.getElementById("mensaje-encriptado").innerHTML = textoDesencriptado;
       mostrar();
       ocultar();
+       }   
 }
 function mostrar(){
-    document.getElementById("caja-resultado").style.display ="flex";
-
+    document.getElementById("container__mensaje_encriptado").style.display ="block";
 }
-
 function ocultar(){
-
     document.getElementById("texto-inicial").style.display = "none";
 }
-
 function copyToClickBoard(){
     var content = document.getElementById('mensaje-encriptado').innerHTML;
 
